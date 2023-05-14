b3b3a078ae0d544e35cb67accd154157a75ba598
SweetyAnime <jnirmal57@gmail.com>
2023-05-14 18:28:31 +0530

    scrape friendly

--- javascript.js
+++ javascript.js
@@ -1,33 +1,29 @@
 function submitForm() {
-  // Retrieve the values from the form inputs
   const ratingValue = document.querySelector('input[name="rating"]:checked').value;
   const usernameValue = document.getElementById('username').value;
   const commentValue = document.getElementById('comment').value;
 
-  // Create a new feedback entry
   const feedbackEntry = document.createElement('div');
   feedbackEntry.classList.add('feedback-entry');
 
-  // Create and append the stars element
   const starsElement = document.createElement('div');
-  starsElement.innerHTML = `Stars: ${getStarSymbols(ratingValue)}`;
+  starsElement.classList.add('stars');
+  starsElement.textContent = getStarSymbols(ratingValue);
   feedbackEntry.appendChild(starsElement);
 
-  // Create and append the username element
   const usernameElement = document.createElement('div');
-  usernameElement.innerHTML = `Username: ${usernameValue}`;
+  usernameElement.classList.add('username');
+  usernameElement.textContent = `Username: ${usernameValue}`;
   feedbackEntry.appendChild(usernameElement);
 
-  // Create and append the comment element
   const commentElement = document.createElement('div');
-  commentElement.innerHTML = `Comment: ${commentValue}`;
+  commentElement.classList.add('comment');
+  commentElement.textContent = `Comment: ${commentValue}`;
   feedbackEntry.appendChild(commentElement);
 
-  // Append the feedback entry to the feedback section
   const feedbackSection = document.getElementById('feedback');
   feedbackSection.appendChild(feedbackEntry);
 
-  // Clear the form inputs
   document.getElementById('username').value = '';
   document.getElementById('comment').value = '';
 }
